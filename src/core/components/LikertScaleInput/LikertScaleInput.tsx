8dd1761a506c5b2ed0fcac98e77e882e5724606a
nathan <nathannash11@gmail.com>
2021-08-22 22:14:38 +0100

    likert scale labels

--- src/core/components/LikertScaleInput/LikertScaleInput.tsx
+++ src/core/components/LikertScaleInput/LikertScaleInput.tsx
@@ -2,38 +2,65 @@ import { FC } from "react";
 import styles from "./LikertScaleInput.module.css";
 
 type Props = {
-	questionText: String;
+	questionText: string;
 	registerField: Object;
 };
 
 type optionProps = {
 	labelText: string;
 	registerField: Object;
+	questionText: string;
 };
 
-const Option: FC<optionProps> = ({ labelText, registerField }) => {
+const Option: FC<optionProps> = ({
+	labelText,
+	registerField,
+	questionText,
+}) => {
 	return (
 		<div className={styles.option}>
-			<label>{labelText}</label>
-			<input type="radio" name="likert" value={labelText} {...registerField} />
+			<label htmlFor={labelText + questionText}>{labelText}</label>
+			<input
+				type="radio"
+				name="likert"
+				value={labelText}
+				id={labelText + questionText}
+				{...registerField}
+			/>
 		</div>
 	);
 };
 
-// TODO does name need to be the question name / input name??
 const LikertScaleInput: FC<Props> = ({ questionText, registerField }) => {
 	return (
 		<div className={styles.likertContainer}>
 			<label className={styles.questionText}>{questionText}</label>
 			<div className={styles.optionsContainer}>
-				<Option registerField={registerField} labelText="Strongly disagree" />
-				<Option registerField={registerField} labelText="Disagree" />
+				<Option
+					registerField={registerField}
+					labelText="Strongly disagree"
+					questionText={questionText}
+				/>
+				<Option
+					registerField={registerField}
+					labelText="Disagree"
+					questionText={questionText}
+				/>
 				<Option
 					registerField={registerField}
 					labelText="Neither agree nor disagree"
+					questionText={questionText}
 				/>
-				<Option registerField={registerField} labelText="Agree" />
-				<Option registerField={registerField} labelText="Strongly agree" />
+				<Option
+					registerField={registerField}
+					labelText="Agree"
+					questionText={questionText}
+				/>
+				<Option
+					registerField={registerField}
+					labelText="Strongly agree"
+					questionText={questionText}
+				/>
 			</div>
 		</div>
 	);
